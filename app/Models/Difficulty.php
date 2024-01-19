c93e38df9938e12aa438723e8de7c517dc1081e2
adams <adamstano5@gmail.com>
2024-01-19 16:14:55 +0100

    difficulty_model

---
+++ app/Models/Difficulty.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Aginev\SearchFilters\Filterable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Difficulty extends Model
+{
+    use HasApiTokens, HasFactory, Notifiable, Filterable;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public function setFilters()
+    {
+        $this->filter->like('name')
+            ->like('created_at');
+    }
+}
